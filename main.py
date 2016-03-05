455ced7aca9af1fbfaffdf5a35a5cae5e5d93d9a
Gabry <gabriele.cimato@gmail.com>
2016-03-05 13:34:17 -0800

    Add show filter based on title
    This is just a first attempt for a perfect match, you can find more
    about it in comments.

--- main.py
+++ main.py
@@ -5,14 +5,20 @@ import time
 
 eztv_rss = open('ezrss.xml')
 eztv_doc = minidom.parse(eztv_rss)
-itemlist = eztv_doc.getElementsByTagName('item')
+show_list = eztv_doc.getElementsByTagName('item')
 pythons_psutil = []
+my_shows = ['Stephen Colbert', 'Conan', 'Jimmy Fallon']
+magnets = []
 
+# Loop needs to be changed so that every 'x' amount of time the app check
+# if the process is active and only then compute, otherwise it needs to sleep
+# for 'x' seconds.
 for p in psutil.process_iter():
     if p.name() == 'openvpn-gui.exe':
-        for s in itemlist:
-            magnet_link = s.getElementsByTagName('torrent:magnetURI')[0].childNodes[0].nodeValue
-            # os.startfile(magnet_link)
-            print(magnet_link)
-    else:
-        time.sleep(10)
+        for show in show_list:
+            title = show.getElementsByTagName('title')[0].childNodes[0].nodeValue
+            # title needs to be filtered using regex to filter out only the title with nothing else
+            # this way only the shows that are in the user favourites will be then processed
+            if title in my_shows:
+                # magnet_link = s.getElementsByTagName('torrent:magnetURI')[0].childNodes[0].nodeValue
+                # os.startfile(magnet_link)
