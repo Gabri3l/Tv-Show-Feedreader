31d437c5495cbfd90fb7d8bd0c4575b5cb5a35cd
Gabry <gabriele.cimato@gmail.com>
2016-03-04 17:39:33 -0800

    Add vpn software control

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from xml.dom import minidom
 import os
+import psutil
+import time
 
 eztv_rss = open('ezrss.xml')
 eztv_doc = minidom.parse(eztv_rss)
 itemlist = eztv_doc.getElementsByTagName('item')
+pythons_psutil = []
 
-
-for s in itemlist:
-    magnet_link = s.getElementsByTagName('torrent:magnetURI')[0].childNodes[0].nodeValue
-    # os.startfile(magnet_link)
-    print(magnet_link)
+for p in psutil.process_iter():
+    if p.name() == 'openvpn-gui.exe':
+        for s in itemlist:
+            magnet_link = s.getElementsByTagName('torrent:magnetURI')[0].childNodes[0].nodeValue
+            # os.startfile(magnet_link)
+            print(magnet_link)
+    else:
+        time.sleep(10)
