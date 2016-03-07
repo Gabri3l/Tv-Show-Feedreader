ed9f635b96500f8e509bc99dfbbcdf1da0a3b1ac
Gabry <gabriele.cimato@gmail.com>
2016-03-06 16:40:49 -0800

    Add regex to filter show title

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ from xml.dom import minidom
 import os
 import psutil
 import time
+import re
 
 eztv_rss = open('ezrss.xml')
 eztv_doc = minidom.parse(eztv_rss)
 show_list = eztv_doc.getElementsByTagName('item')
 pythons_psutil = []
-my_shows = ['Stephen Colbert', 'Conan', 'Jimmy Fallon']
+my_shows = ['Stephen Colbert', 'Conan', 'Jimmy Fallon', 'Yukon Gold']
 magnets = []
 
 # Loop needs to be changed so that every 'x' amount of time the app check
@@ -17,8 +18,17 @@ for p in psutil.process_iter():
     if p.name() == 'openvpn-gui.exe':
         for show in show_list:
             title = show.getElementsByTagName('title')[0].childNodes[0].nodeValue
-            # title needs to be filtered using regex to filter out only the title with nothing else
-            # this way only the shows that are in the user favourites will be then processed
-            if title in my_shows:
+            try:
+                # This is a temp regex solution that looks for patterns like s09e01 which
+                # is the standard when namin a tv show file
+                match = re.search(r's[0-9]{2}e[0-9]{2}', title, re.IGNORECASE).group(0)
+                # Once the regex catches the interesting part, the title will be filtered, every word of it capitalized
+                # and whitespace trimmed
+                filtered_title = title.split(match)[0].title().strip()
+            except AttributeError:
+                filtered_title = title
+
+            if filtered_title in my_shows:
+                print filtered_title
                 # magnet_link = s.getElementsByTagName('torrent:magnetURI')[0].childNodes[0].nodeValue
                 # os.startfile(magnet_link)
