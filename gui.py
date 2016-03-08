682661acb8f5a2b7766172342c9704a250de41c3
Gabry <gabriele.cimato@gmail.com>
2016-03-07 17:56:55 -0800

    Add start button and vpn check method

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 import wx
+import psutil
 
 
 class WindowClass(wx.Frame):
@@ -11,18 +12,48 @@ class WindowClass(wx.Frame):
 
     def add_gui(self):
 
+        def check_vpn():
+            vpn_app = vpn_text_area.GetValue()  # This value needs to be filtered with a regex
+            is_vpn_active = False
+            if vpn_app == '':
+                vpn_text_area.SetValue('Please provide a valid application path')
+                print 'no vpn name specified'
+            elif vpn_app == 'Please provide a valid application path':
+                print 'no vpn name specified'
+            else:
+                for p in psutil.process_iter():
+                    if p.name() == 'openvpn-gui.exe':
+                        is_vpn_active = True
+
+                if is_vpn_active:
+                    print "vpn is active"
+                else:
+                    print 'vpn is not active'
+            return is_vpn_active
+
         def on_button(event):
             open_file_dialog = wx.FileDialog(self, "Open", "", "",
                                              "Executables (*.exe)|*.exe",
                                              wx.FD_OPEN | wx.FD_FILE_MUST_EXIST)
             open_file_dialog.ShowModal()
-            text_area.SetValue(open_file_dialog.GetPath())
+            vpn_text_area.SetValue(open_file_dialog.GetPath())
+
+        def on_start(event):
+            check_vpn()
 
         panel = wx.Panel(self, wx.ID_ANY)
-        button = wx.Button(panel, wx.ID_ANY, 'Select VPN', (250, 10))
-        text_area = wx.TextCtrl(panel, pos=(10, 10), size=(230, 25))
-        # dlg = wx.TextEntryDialog(panel, 'Enter text', 'some text', pos=(10,10))
-        button.Bind(wx.EVT_BUTTON, on_button)
+
+        # buttons
+        open_file_button = wx.Button(panel, wx.ID_ANY, 'Select VPN', (530, 10))
+        start_button = wx.Button(panel, wx.ID_ANY, 'Start', (250, 390))
+
+        # buttons bindings
+        open_file_button.Bind(wx.EVT_BUTTON, on_button)
+        start_button.Bind(wx.EVT_BUTTON, on_start)
+
+        vpn_text_area = wx.TextCtrl(panel, 2, pos=(10, 10), size=(510, 25))
+        vpn_text_area.Disable()
+
 
 if __name__ == '__main__':
     app = wx.App()
