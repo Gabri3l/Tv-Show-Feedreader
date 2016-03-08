4397f2970ddb97c955102b89ebe09f096df679af
Gabry <gabriele.cimato@gmail.com>
2016-03-07 17:30:55 -0800

    Save open file path in text area

--- gui.py
+++ gui.py
@@ -16,9 +16,11 @@ class WindowClass(wx.Frame):
                                              "Executables (*.exe)|*.exe",
                                              wx.FD_OPEN | wx.FD_FILE_MUST_EXIST)
             open_file_dialog.ShowModal()
+            text_area.SetValue(open_file_dialog.GetPath())
 
         panel = wx.Panel(self, wx.ID_ANY)
         button = wx.Button(panel, wx.ID_ANY, 'Select VPN', (250, 10))
+        text_area = wx.TextCtrl(panel, pos=(10, 10), size=(230, 25))
         # dlg = wx.TextEntryDialog(panel, 'Enter text', 'some text', pos=(10,10))
         button.Bind(wx.EVT_BUTTON, on_button)
 
