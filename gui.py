59e0c537d9f0bdd1d3897c4cae956799951b6f5d
Gabry <gabriele.cimato@gmail.com>
2016-03-08 17:44:11 -0800

    Allow to add and remove shows

--- gui.py
+++ gui.py
@@ -38,6 +38,35 @@ class WindowClass(wx.Frame):
             open_file_dialog.ShowModal()
             vpn_text_area.SetValue(open_file_dialog.GetPath())
 
+        def on_add_show(event):
+            if show_box.ShowModal() == wx.ID_OK:
+                new_show = show_box.GetValue()
+                if tv_show_text_area.GetValue() == '':
+                    tv_show_text_area.SetValue(new_show.title().strip())
+                else:
+                    current_shows = tv_show_text_area.GetValue().split(', ')
+                    if new_show in current_shows:
+                        print 'show exists already'
+                    else:
+                        tv_show_text_area.AppendText(', ' + new_show.title().strip())
+            show_box.SetValue('Show name')
+
+        def on_del_show(event):
+            if del_show_box.ShowModal() == wx.ID_OK:
+                to_del_show = del_show_box.GetValue().title().strip()
+                current_shows = tv_show_text_area.GetValue().split(', ')
+                if to_del_show not in current_shows:
+                    print to_del_show + ' show not in list'
+                else:
+                    current_shows.remove(to_del_show)
+                    tv_show_text_area.SetValue('')
+                    for index, show in enumerate(current_shows):
+                        if index < len(current_shows) - 1:
+                            tv_show_text_area.AppendText(show + ', ')
+                        else:
+                            tv_show_text_area.AppendText(show)
+            del_show_box.SetValue('Show name')
+
         def on_start(event):
             check_vpn()
 
@@ -45,15 +74,25 @@ class WindowClass(wx.Frame):
 
         # buttons
         open_file_button = wx.Button(panel, wx.ID_ANY, 'Select VPN', (530, 10))
+        add_show_button = wx.Button(panel, wx.ID_ANY, 'Add Show', (440, 45))
+        del_show_button = wx.Button(panel, wx.ID_ANY, 'Del Show', (530, 45))
         start_button = wx.Button(panel, wx.ID_ANY, 'Start', (250, 390))
 
         # buttons bindings
         open_file_button.Bind(wx.EVT_BUTTON, on_button)
+        add_show_button.Bind(wx.EVT_BUTTON, on_add_show)
+        del_show_button.Bind(wx.EVT_BUTTON, on_del_show)
         start_button.Bind(wx.EVT_BUTTON, on_start)
 
+        # text areas
         vpn_text_area = wx.TextCtrl(panel, 2, pos=(10, 10), size=(510, 25))
         vpn_text_area.Disable()
+        tv_show_text_area = wx.TextCtrl(panel, 2, pos=(10, 45), size=(420, 25))
+        tv_show_text_area.Disable()
 
+        # modal
+        show_box = wx.TextEntryDialog(None, 'What show would you like to add?', 'Add Show', 'Show name')
+        del_show_box = wx.TextEntryDialog(None, 'Which show would you like to remove?', 'Del Show', 'Show name')
 
 if __name__ == '__main__':
     app = wx.App()
